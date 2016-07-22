c35a56e8048f11ef75d0460d4597e7bb6cbf9338
Thomas-Nogueira <thomas.nogueira@hotmail.com>
2016-07-22 10:36:10 -0300

    Add files via upload

---
+++ musica.asm
@@ -0,0 +1,309 @@
+	
+	
+maior:
+	add $8, $8, 1
+	beq $8, 3, corinho
+
+	li $2,31
+	li $4, 67
+	li $5 1000
+	li $6,1
+	li $7,100
+	syscall
+
+	li $2,31
+	li $4, 67
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 10000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 76
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 76
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 10000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 73
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 73
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 71
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 71
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 69
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 69
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 68
+	li $5 10000
+	li $6,0
+	li $7,100
+	syscall
+	
+	j	maior
+	
+corinho:
+	addi $8, $0, 0 
+	
+	li $2,31
+	li $4, 74
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 73
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 73
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 71
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 71
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 69
+	li $5 10000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 73
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 73
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 71
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 71
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 69
+	li $5 10000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 67
+	li $5 1000
+	li $6,1
+	li $7,100
+	syscall
+
+	li $2,31
+	li $4, 67
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 10000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 76
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 76
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 74
+	li $5 10000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 73
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 73
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 71
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 71
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 69
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 69
+	li $5 1000
+	li $6,0
+	li $7,100
+	syscall
+	
+	li $2,31
+	li $4, 68
+	li $5 10000
+	li $6,0
+	li $7,100
+	syscall
+	
+	j	 maior
+	
+sai:
+	nop	
